609fdf9b19278015f3c19145c149722d6a399b39
Joao Moreira <joao.moreira@oneserve.co.uk>
2018-01-30 15:50:20 +0000

    Added regex to check against a valid playlist url

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 chrome.browserAction.onClicked.addListener((tab) => {
-    if (tab.url.indexOf('https://soundcloud.com') !== -1) {
+    const scRegex = /^http(s)?:\/\/soundcloud.com\/(\w|-|_)+\/sets\/(\w|-|_)+$/;
+    if (scRegex.test(tab.url)) {
         chrome.tabs.executeScript(null, { file: 'shuffle.js' });
     } else {
         const url = window.prompt('Enter URL to Soundcloud Playlist', 'https://soundcloud.com/disco4dummies/sets/work-mixtapes');
